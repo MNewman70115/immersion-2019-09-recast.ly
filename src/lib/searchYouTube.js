49ed3d9df2e8b67e9803f698801adf50a8ee7872
Matthew Newman <matt.newman@gmail.com>
2019-10-04 01:46:05 -0500

    got searchYouTube working

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,71 @@
-const searchYouTube = (options, callback) => {
-  // TODO
+// const searchYouTube = (options, callback) => {
+//   // TODO
+//   var url = 'https://www.googleapis.com/youtube/v3/search';
+
+//   $.ajax({
+//     url: url,
+//     type: 'GET',
+//     data: {
+//       part: 'id,snippet',
+//       q: options.query,
+//       id: options.vidId,
+//       maxResults: 5,
+//       type: 'video',
+//       key: window.YOUTUBE_API_KEY,
+//       videoEmbeddable: true,
+//     },
+//     dataType: 'jsonp',
+//     success: function (data) {
+//       //console.log('successful fetch from server: ', data);
+//       callback(data);
+//     },
+//     error: function (data) {
+//       console.error('failed to send ajax request', data);
+//     }
+//   });
+
+// };
+
+// var searchYouTube = (options, callback) => {
+//   let url = 'https://www.googleapis.com/youtube/v3/search';
+//   let hasCallback = typeof callback === 'function';
+//   let videos = [];
+
+//   $.get('https://www.googleapis.com/youtube/v3/search',
+//     {
+//       part: 'snippet',
+//       maxResults: options.max,
+//       q: options.query,
+//       key: options.key,
+//       type: 'video',
+//     }
+//   ).done((data) => { callback(data.items); console.log('I twas successful!'); }).fail(function () { console.log('i failed'); });
+
+
+// };
+
+const searchYouTube = ({ key, query, max = 5 }, callback) => {
+  console.log(key, query);
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    part: 'snippet',
+    key: key,
+    q: query,
+    maxResults: max,
+    type: 'video',
+    videoEmbeddable: 'true'
+  })
+    .done(({ items }) => {
+      if (callback) {
+        console.log(items);
+        callback(items);
+      }
+    })
+    .fail(({ responseJSON }) => {
+      responseJSON.error.errors.forEach((err) =>
+
+        console.error(err)
+      );
+    });
 };
 
 window.searchYouTube = searchYouTube;
