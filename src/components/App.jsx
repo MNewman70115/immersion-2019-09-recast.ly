2642818bfb03feb97bd8ca74fcb1991aec24b0d5
Matthew Newman <matt.newman@gmail.com>
2019-10-04 01:41:54 -0500

    Got App.jsx working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,80 @@
-const App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+class App extends React.Component {
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
-window.App = App;
+  // initilize() {
+  // this.props.searchYouTube();
+  // }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      video: {}
+    };
+
+    this.changeVideo = this.changeVideo.bind(this);
+    this.search = this.search.bind(this);
+
+  }
+
+  componentDidMount() {
+    var self = this;
+
+    this.props.searchYouTube({}, function (videos) {
+      self.setState({
+        videos: videos,
+        video: videos[0]
+      });
+    });
+
+  }
+
+  changeVideo(video) {
+
+    this.setState({
+      video: video
+    });
+
+  }
+
+  search(query) {
+
+    var self = this;
+
+    this.props.searchYouTube({
+      query: query
+    }, function (videos) {
+
+      self.setState({
+        videos: videos
+      });
+
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search search={this.search} />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.video} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos} changeVideo={this.changeVideo} />
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+
+}
+  // In the ES6 spec, files are "modules" and do not share a top-level scope
+  // `var` declarations will only exist globally where explicitly defined
+  window.App = App;
