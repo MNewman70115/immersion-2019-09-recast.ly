d683b99312614a3e89c6f86f5c0cd636faea393a
Matthew Newman <matt.newman@gmail.com>
2019-10-04 01:47:11 -0500

    setup index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 // TODO: Render the `App` component to the DOM
+// const throttledSearch = _.throttle(searchYouTube, 500);
+// ReactDOM.render(<App video={} search={throttledSearch} />, document.getElementById('app'));
+
+ReactDOM.render(<App searchYouTube={searchYouTube} />, document.getElementById('app'));
